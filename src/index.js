32eeb67893ccf729b67ee0ea6d1af52aeb7df9b5
jhmwkg <thejoepage@gmail.com>
2026-02-28 18:10:32 -0500

    Import bootstrap CSS to activate navbar collapse

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import {
