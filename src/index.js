b11937da4398a36e2f62edfd241de4f85ef421a8
jhmwkg <thejoepage@gmail.com>
2026-02-28 18:03:56 -0500

    Add header with router dropdown navigation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from 'react-router-dom';
+import HomePage from './components/HomePage';
+import FocusTracker from './components/FocusTracker';
+
+function Header() {
+  const [open, setOpen] = useState(false);
+  const location = useLocation();
+
+  const toggle = () => setOpen(o => !o);
+  const close = () => setOpen(false);
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          FocusApp
+        </Link>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            onClick={toggle}
+            aria-expanded={open}
+          >
+            Navigate
+          </button>
+          <ul className={
+            'dropdown-menu' + (open ? ' show' : '')
+          }>
+            <li>
+              <Link
+                className={
+                  'dropdown-item' + (location.pathname === '/' ? ' active' : '')
+                }
+                to="/"
+                onClick={close}
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                className={
+                  'dropdown-item' + (location.pathname === '/tracker' ? ' active' : '')
+                }
+                to="/tracker"
+                onClick={close}
+              >
+                Tracker
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function Root() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tracker" element={<FocusTracker />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// performance measurement (unchanged)
 reportWebVitals();
