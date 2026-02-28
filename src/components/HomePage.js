e81be626e35370691fb0072a87bdcc300286e988
jhmwkg <thejoepage@gmail.com>
2026-02-28 18:03:56 -0500

    Add header with router dropdown navigation

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,14 @@
 import React from 'react';
-import FocusTracker from './FocusTracker';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
-  return <FocusTracker />;
+  return (
+    <div className="text-center py-4">
+      <h1>Home Page</h1>
+      <p>Welcome to the focus tracker demo.</p>
+      <Link to="/tracker" className="btn btn-primary">
+        Go to Tracker
+      </Link>
+    </div>
+  );
 }
